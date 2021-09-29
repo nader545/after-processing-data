587da5cbf1dd5b744c6e13c0626b3f712d7212f3
Nader <nadrmhmd057@gmail.com>
2021-09-29 10:42:42 +0200

    tack

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_file = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_file
+            // 
+            this.btn_file.BackColor = System.Drawing.Color.Chocolate;
+            this.btn_file.Location = new System.Drawing.Point(81, 291);
+            this.btn_file.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(328, 44);
+            this.btn_file.TabIndex = 0;
+            this.btn_file.Text = "after processing data";
+            this.btn_file.UseVisualStyleBackColor = false;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txt_path.Location = new System.Drawing.Point(81, 160);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(328, 33);
+            this.txt_path.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(460, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "inputFilePath";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 469);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.btn_file);
+            this.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_file;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            int column1;
+            string column2, column3;  
+            string date_file = txt_path.Text;
+            string inputFilePath = "E:\\" + date_file + ".pu";                      
+            DataTable dt = new DataTable();
+            dt = CreateDataTableFromFile(inputFilePath, 15, 1);
+            string count = dt.Rows[0][1].ToString();
+            string date = dt.Rows[0][0].ToString();
+
+            if (date != date_file)
+            {
+                MessageBox.Show("invalid file !: file name don’t match with date in first line");
+                return;
+            }
+            if (Convert.ToInt32(count) != Convert.ToInt32(dt.Rows.Count)-1)
+            {
+                MessageBox.Show("invalid file !:count on first line don’t match with lines count -1");
+                return;
+            }
+            string OutputFilePath = (@"E:\" + date_file + ".ur");
+            StreamWriter file = new StreamWriter(OutputFilePath);
+            for (int i = 1; i < Convert.ToInt32(dt.Rows.Count); i++)
+            {
+                try
+                {
+                    column1 = Convert.ToInt32(dt.Rows[i][0]);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("invalid file");                    
+                    return;
+                }
+                column2 = dt.Rows[i][1].ToString();
+                column3 = dt.Rows[i][2].ToString();
+                file.WriteLine(OutputFilePath, (column1 + "," + column2 + "," + column3));
+            }               
+        }
+
+
+
+//-------------------------------------------------------------------------------------------------------    
+        private DataTable CreateDataTableFromFile(string inputFilePath, int count, int file_extension)
+        {
+
+            int lines;
+            DataTable dt = new DataTable();
+            DataColumn dc;
+            DataRow dr;
+            if (file_extension == 1)
+            {
+                lines = count;
+                for (int i = 1; i <= lines; i++)
+                {
+                    dc = new DataColumn();
+                    dc.DataType = System.Type.GetType("System.String");
+                    dc.ColumnName = string.Format("f{0}", i);
+                    dc.Unique = false;
+                    dt.Columns.Add(dc);
+                }
+
+                StreamReader file = new StreamReader(@inputFilePath, Encoding.Default);
+                string input;
+                while ((input = file.ReadLine()) != null)
+                {
+                    //string[] s = input.Split(new string[] { "||" }, StringSplitOptions.None);
+                    string[] arr = input.Split(new char[] { ',' });
+                    dr = dt.NewRow();
+
+                    for (int i = 1; i <= lines; i++)
+                    {
+                        string col = string.Format("f{0}", i);
+                        try
+                        {
+                            dr[col] = arr[i - 1];
+
+                        }
+                        catch (Exception e) { }
+
+                    }
+                    dt.Rows.Add(dr);
+
+                }
+                file.Close();
+            }          
+            return dt;
+        }
+
+    }
+}
